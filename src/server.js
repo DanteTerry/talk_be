b88e42bc5601c5731bfd73d60567c4f8072d75a2
DanteTerry <sinasa88@gmail.com>
2024-06-22 13:04:00 +0200

    handled unexpected exception and Rejection

--- src/server.js
+++ src/server.js
@@ -8,6 +8,33 @@ dotenv.config();
 // env Variable
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   logger.info(`Server is listing on PORT ${PORT}..`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+// handle server errors
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+// handling unexpected exception and Rejection
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// sigterm
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
