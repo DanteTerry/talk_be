3cb9540a99fe1e318cda2b5b317f803a30d15535
DanteTerry <sinasa88@gmail.com>
2024-06-22 15:03:20 +0200

    added mongodb connection to project and handled mongodb connection error

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import logger from "./configs/logger.js";
+import mongoose from "mongoose";
 
 // dotEnv Config
 dotenv.config();
 
 // env Variable
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const { MONGO_URL } = process.env;
 
+// mongodb connection
+mongoose.connect(MONGO_URL).then(() => {
+  logger.info("Connected to MONGODB");
+});
+
+// exit on mongodb connection error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error : ${err}`);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+// listing to server
 const server = app.listen(PORT, () => {
   logger.info(`Server is listing on PORT ${PORT}..`);
 });
